9d41b9717f3bf3a14808930aa38454d4c6bd2f8c
Marnee Irby <irbymarnee@gmail.com>
2022-10-05 16:30:34 -0700

    Day 1 Take Home Challenge:Added single line and multi-line comments and added console logs for name and favorite food item

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* 
+TODO:
+-Finish Take Home Review
+-Move on to git review
+(Multi-line Comment)
+*/
+
+// Hello, my name is Marnee (single line comment)
+
+console.group();
+console.log("Marnee");
+console.log("French Fries");
+console.groupEnd();
+console.log("Not in a Group!");
